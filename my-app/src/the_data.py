a0a664ea9526ca4da68996b17db8d8134afb3064
Eric Taylor <ericfromspringfield@gmail.com>
2023-10-30 20:39:15 +0000

    Added proxy to package.json and added the_data.py file

---
+++ my-app/src/the_data.py
@@ -0,0 +1,27 @@
+# import Flask to set up back-end to call ESPN API - front-end will call this back-end for score and game data
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import requests
+
+app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
+
+# You can use this backend by making requests to /proxy
+# and providing the target URL as a query parameter.
+# For example:
+# http://localhost:5000/proxy?url=http://yourapi.com/data
+@app.route('/proxy', methods=['GET'])
+def proxy():
+    # Get the target URL from the query parameter
+    target_url = request.args.get('url')
+
+    if not target_url:
+        return jsonify({'error': 'No URL provided'}), 400
+
+    response = requests.get(target_url)
+
+    return jsonify(response.json())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
