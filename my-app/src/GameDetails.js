b44ce31cb8147d35e1d5dc90b714eb9992980bce
Eric Taylor <ericfromspringfield@gmail.com>
2023-11-03 21:31:34 +0000

    Customized GameDetails and Scoreboard

--- my-app/src/GameDetails.js
+++ my-app/src/GameDetails.js
@@ -31,9 +31,12 @@ function GameDetails() {
   const venuePhoto2 = gameDetails?.gameInfo?.venue?.images[1]?.href
   const dateTime = gameDetails.header.competitions[0].status.type.detail
   const network = gameDetails.header.competitions[0].broadcasts[0].media.shortName
-  const weather = gameDetails.gameInfo.weather.temperature
+  // const weather = gameDetails.gameInfo.weather.temperature
+  const city = gameDetails.gameInfo.venue.address.city
+  const state = gameDetails.gameInfo.venue.address.state
 
   const awayLogo = gameDetails.boxscore.teams[0].team.logo
+  const awayAltLogo = gameDetails.header.competitions[0].competitors[1].team.logos[1].href
   const awayTeam = gameDetails.header.competitions[0].competitors[1].team.displayName 
   const awayScore = gameDetails.header.competitions[0].competitors[1].score
   const awayColor = gameDetails.header.competitions[0].competitors[1].team.color
@@ -43,6 +46,7 @@ function GameDetails() {
 
 
   const homeLogo = gameDetails.boxscore.teams[1].team.logo
+  const homeAltLogo = gameDetails.header.competitions[0].competitors[0].team.logos[1].href
   const homeTeam = gameDetails.header.competitions[0].competitors[0].team.displayName
   const homeScore = gameDetails.header.competitions[0].competitors[0].score
   const homeColor = gameDetails.header.competitions[0].competitors[0].team.color
@@ -56,9 +60,21 @@ function GameDetails() {
   return (
     <div>
       {/* <img src={venuePhoto2} alt={venue}></img> */}
-      <h2>{venue} {dateTime} {network}</h2>
-      <h2 style={{color: awayTeamColor}} className="awayTeamGame"><img src={awayLogo} width={"100"} height={"100"} align={"center"} alt="teamLogo"></img>{awayRank} {awayTeam} ({awayRecord}, {awayConferenceRecord}) {awayScore}</h2>
-      <h2 style={{color: homeTeamColor}} className="homeTeamGame"><img src={homeLogo} width={"100"} height={"100"} align={"center"} alt="teamLogo"></img>{homeRank} {homeTeam} ({homeRecord}, {homeConferenceRecord}) {homeScore}</h2>
+      <h2 style={{marginBottom: "0", textAlign: "center"}}> <img src={awayLogo} width={"100"} height={"100"} align={"center"} alt="teamLogo"></img> {dateTime} | {network} <img src={homeLogo} width={"100"} height={"100"} align={"center"} alt="teamLogo"></img></h2> 
+      <hr style={{color: awayTeamColor, borderBlockColor: awayTeamColor}}></hr>
+      <hr style={{color: awayTeamColor, borderBlockColor: awayTeamColor}}></hr>
+      <h2 style={{textAlign: "center", backgroundColor: homeTeamColor, padding: "20px", color: "whitesmoke", fontSize: "2em"}}>{venue} | {city}, {state}</h2>
+      <hr style={{color: awayTeamColor, borderBlockColor: awayTeamColor}}></hr>
+      <hr style={{color: awayTeamColor, borderBlockColor: awayTeamColor}}></hr>
+
+      <h2 style={{color: awayTeamColor, marginLeft: "5%"}} className="awayTeamGame">({awayRecord}, {awayConferenceRecord}) {awayRank} {awayTeam} {awayScore}</h2>
+      <hr style={{color: awayTeamColor, borderBlockColor: awayTeamColor}}></hr>
+
+      <hr style={{color: homeTeamColor, borderBlockColor: homeTeamColor}}></hr>
+      <h2 style={{color: homeTeamColor, marginLeft: "5%"}} className="homeTeamGame">({homeRecord}, {homeConferenceRecord}) {homeRank} {homeTeam} {homeScore}</h2>
+      <hr style={{color: homeTeamColor, borderBlockColor: homeTeamColor}}></hr>
+      <hr style={{color: homeTeamColor, borderBlockColor: homeTeamColor}}></hr>
+
       {/* ... other details */}
     </div>
   );
